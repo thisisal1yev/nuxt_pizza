9ebc569451e38aec0548cf309b034ab3c6c987f2
Aliyev Axmadillo <aaalievvv1@gmail.com>
2026-06-14 12:36:34 +0500

    feat(admin): add admin API client

--- app/services/apiClient.ts
+++ app/services/apiClient.ts
@@ -3,7 +3,8 @@ import * as ingredients from './ingredients'
 import * as products from './products'
 import * as auth from './auth'
 import * as stories from './stories'
+import * as admin from './admin'
 
 export const Api = {
-	products, ingredients, cart, auth, stories
+	products, ingredients, cart, auth, stories, admin
 }
