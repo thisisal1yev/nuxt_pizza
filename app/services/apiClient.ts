adb697daa379e3447963528c999b63ac42f43b60
Aliyev Axmadillo <aaalievvv1@gmail.com>
2026-06-14 13:17:28 +0500

    chore: remove unused Stories feature (component, service, API, model, seed)

--- app/services/apiClient.ts
+++ app/services/apiClient.ts
@@ -2,9 +2,8 @@ import * as cart from './cart'
 import * as ingredients from './ingredients'
 import * as products from './products'
 import * as auth from './auth'
-import * as stories from './stories'
 import * as admin from './admin'
 
 export const Api = {
-	products, ingredients, cart, auth, stories, admin
+	products, ingredients, cart, auth, admin
 }
