273eb1e0158afee9caa03daa23d9c598abb05588
Aliyev Axmadillo <aaalievvv1@gmail.com>
2026-06-14 13:27:59 +0500

    fix(ui): hide sort popup on mobile (show lg+)

--- app/components/TopBar.vue
+++ app/components/TopBar.vue
@@ -10,7 +10,7 @@ defineProps<{
 	<div class="sticky top-0 bg-white py-5 mt-5 shadow-lg shadow-black/5 z-10">
 		<div class="container flex items-center justify-between gap-3">
 			<Categories :categories />
-			<div class="shrink-0">
+			<div class="shrink-0 hidden lg:block">
 				<SortPopup />
 			</div>
 		</div>
